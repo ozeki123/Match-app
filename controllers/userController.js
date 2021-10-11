8d267ba3263c39309e4046630045fa9485e9e5e7
Andrew Ozeki <andrewozeki@gmail.com>
2021-10-11 18:20:16 +0900

    added: login authentication function

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,12 +35,50 @@ module.exports.signup = (req, res) => {
                                 if(err) throw err;
                                 res.json({
                                     token,
-                                    user: { id: user._id, name: user.name, email: user.email}
-                                })
+                                    user: { id: user._id, 
+                                        name: user.name, 
+                                        email: user.email
+                                    }
+                                });
                             }
                         )
                     })
             })
         })
     })
+}
+
+module.exports.login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if(!email || !password) {
+        res.status(400).json({msg: 'Please enter all fields'});
+    }
+
+    User.findOne({email})
+        .then(user => {
+            if(!user) return res.status(400).json({msg: 'User does not exist.'});
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(!isMatch) return res.status(400).json({ msg: 'Invalid login credentials'})
+
+                    jwt.sign(
+                        { id: user_id },
+                        config.get('jwtsecret'),
+                        { expiresIn: 7200 },
+                        (err, token) => {
+                            if(err) throw err;
+                            res.json({
+                                token,
+                                user: {
+                                    id: user._id,
+                                    name: user.name,
+                                    email: user.email
+                                }
+                            });
+                        }
+                    )
+                })
+        })
 }
